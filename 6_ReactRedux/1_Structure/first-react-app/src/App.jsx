0be35688c27435261f349815c0faad49e2501622
ARAFAT <munshiarafathussain@gmail.com>
2024-02-09 00:48:53 +0600

    React updated

---
+++ 6_ReactRedux/1_Structure/first-react-app/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <h1>Hello World</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
